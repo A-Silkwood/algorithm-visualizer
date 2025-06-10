63135352d3b357864dde0ba005510df2a9abbe74
Andrew Silkwood <asilkwoo@asu.edu>
2025-06-10 09:21:32 -0700

    swap goal and start colors

--- src/features/path-finding/constants.ts
+++ src/features/path-finding/constants.ts
@@ -1,7 +1,7 @@
 export const STATES = {
     EMPTY: '#ffffff',
-    START: '#c26d6e',
-    GOAL: '#75c26d',
+    START: '#75c26d',
+    GOAL: '#c26d6e',
     WALL: '#2f303b',
     PATH: '#6da7c2',
     PATH_SEARCHING: '#a66dc2',
