b5cb27b1a57f156575226b6d0c5e8ffdd8e8c443
Andrew Silkwood <asilkwoo@asu.edu>
2025-06-13 09:37:45 -0700

    color changes; added run states

--- src/features/path-finding/constants.ts
+++ src/features/path-finding/constants.ts
@@ -1,9 +1,17 @@
 export const STATES = {
-    EMPTY: '#ffffff',
-    START: '#75c26d',
-    GOAL: '#c26d6e',
+    EMPTY: '#f0f0f0',
+    START: '#75f073',
+    GOAL: '#f0737f',
     WALL: '#2f303b',
-    PATH: '#6da7c2',
-    PATH_SEARCHING: '#a66dc2',
-    SEARCHED: '#e2e3bf',
+    PATH: '#c7a2de',
+    PATH_SEARCHING: '#e8cc41',
+    SEARCHED: '#f7e7b5',
+    FOUND: '#73e8f0',
+}
+
+export const RUN_STATE = {
+    NONE: 'NONE',
+    STARTED: 'STARTED',
+    PAUSED: 'PAUSED',
+    FINISHED: 'FINISHED',
 }
